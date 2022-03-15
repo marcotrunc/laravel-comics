59913a5fa57eef627b8ea2e332f3773ba7ca78c8
marcotrunc <marcotrunc@gmail.com>
2022-03-15 20:19:23 +0100

    completed the basic layout

--- resources/views/comics_page.blade.php
+++ resources/views/comics_page.blade.php
@@ -1,3 +1,8 @@
+@php
+$current_series = config('products')    
+@endphp
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,9 +15,34 @@
 <body>
     {{-- Header --}}
    @include('includes.header')
+
    {{-- Jumbo --}}
    <section class="jumbo"></section>
+
    {{-- Main --}}
+    <main> 
+        <div class="container py-5">
+            {{-- Titolo --}}
+            <div class="title-curr-ser">
+                <a href="" class="text-uppercase">Current Series</a>
+            </div>
+            {{-- Card --}}
+            <div class="row">
+                @forelse ($current_series as $comic)
+                <div class="col-2">
+                    <div class="card" role="button">
+                        <img src="{{$comic['thumb']}}" class="card-img-top" alt="{{ $comic['title'] }}">
+                        <div class="card-body">
+                          <h6 class="card-title text-uppercase">{{ $comic['series'] }}</h6>
+                        </div>
+                      </div>
+                </div>
+                @empty
+                <p>Non risultano fumetti disponibili</p>
+                @endforelse
+            </div>
+        </div>
+    </main>
    {{-- Footer --}}
    @include('includes.footer')
   
